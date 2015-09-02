177e1c7254154b3ecf580630a867feb499fc5649
Ilias Tsangaris <ilias@ga.co>
2015-09-02 09:44:46 -0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
   email: {type: String, required: true},
   passwordDigest: {type: String, required: true},
-  createdAt: {type: Date, required: true}
+  createdAt: {type: Date, default: Date.now}
 });
 
 // create a new user with secure (hashed) password (for sign up)
@@ -34,16 +34,17 @@ UserSchema.statics.createSecure = function (email, password, cb) {
 UserSchema.statics.authenticate = function (email, password, cb) {
   // find user by email entered at log in
   this.findOne({email: email}, function (err, user) {
-    
     // throw error if can't find user
     if (user === null) {
-      throw new Error('Can\'t find user with email ' + email);
-
+      cb("Can\'t find user with that email", null);
     // if found user, check if password is correct
     } else if (user.checkPassword(password)) {
       // the user is found & password is correct, so execute callback
       // pass no error, just the user to the callback
       cb(null, user);
+    } else {
+      // user found, but password incorrect
+      cb("password incorrect", user)
     }
   });
 };
@@ -56,19 +57,8 @@ UserSchema.methods.checkPassword = function (password) {
   return bcrypt.compareSync(password, this.passwordDigest);
 };
 
-// checks if a user is valid
-UserSchema.methods.trySave = function() {
-  this.save(function(err, user) {
-    if (err === null) {
-      console.log(user + "\nsuccessfully saved!")
-    } else {
-      console.log("Error saving: " + err.message)
-    }
-  })
-}
-
 // define user model
 var User = mongoose.model('User', UserSchema);
 
- // export user model
+// export user model
 module.exports = User;
