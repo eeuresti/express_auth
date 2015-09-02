be5c3b0635d71d6933902fb0acc676f599b24e93
Ilias Tsangaris <iliastsangaris@gmail.com>
2015-09-01 18:43:11 -0700

    step 4: signup route

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
+    db = require("./models"),
     app = express();
 
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/signup", function (req, res) {
   res.send("Coming soon");
+});
+
+// where the user submits the sign-up form
+app.post(["/users", "/signup"], function signup(req, res) {
+  // grab the user from the params
+  var user = req.body.user;
+  // pull out their email & password
+  var email = user.email;
+  var password = user.password;
+  // create the new user
+  db.User.createSecure(email, password, function() {
+    res.send(email + " is registered!\n");
+  });
 });
 
 var listener = app.listen(3000, function () {
