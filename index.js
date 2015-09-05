373a351c200c2fa44a8bd41f7879f2f5d736cbc6
Eric Euresti <eeuresti@gmail.com>
2015-09-05 15:58:17 -0700

    update changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/signup", function (req, res) {
   res.send("Coming soon");
